ebabca7f2333124d6a4ed91a675fef061c7de7c2
Strelok_2012 <uraniumpuppy@gmail.com>
2024-02-13 20:27:03 +1100

    Nuking the linked list
    For reasons which I will never know the #defined linked list didnt't work with my code. No idea why but I need to get a move on with this so I can actually begin writing some Vulkan code. Instead I'm going to try and brute force it and not try to do it in a clever way.

--- memasta.c
+++ memasta.c
@@ -1,12 +1,11 @@
 #include "memasta.h"
 
+
 void* mm_alloc(size_t size)
 {
 
     printf("attempting to allocate %zu bytes of memory \n", size);
     int *ptr = malloc(size);
-    // printf("int* ptr: %p \n", ptr);
-    // int* ptr = malloc(size);
     if(!ptr)
     {
         printf("Failed to allocate %zu bytes of memory, out of memory? \n", size);
@@ -14,34 +13,22 @@ void* mm_alloc(size_t size)
     struct mm_seg* seg = (void*)ptr;
     seg->mem = ptr;
     seg->size = size;
-
-    // printf("ptr: %p \n", ptr);
-
-    // void* seg_r = ptr + sizeof *seg; // Unknown why it is necessary to create a new pointer by moving the pointer of ptr by the size of seg
-
-    // printf("seg_r: %p \n", seg_r);
+    seg->next = NULL;
+    seg->prev = NULL;
 
     return seg;
 }
+
 
 void mm_free(void* seg)
 {
     struct mm_seg* seg_p = (struct mm_seg*) seg;
     // Potential for some kind of check that checks for pointer type?
     printf("Freeing %zu bytes of memory \n", seg_p->size);
-    // void* seg_v = seg;
-    // printf("Freeing an undetermined amount of memory \n");
     free(seg);
-    // seg = NULL;
 }
 
 int main(void)
 {
-    struct mm_seg *test_seg = mm_alloc(64);
-
-    // printf("Seg ptr: %p \n", test_seg);
-
-    mm_free(test_seg);
-
     printf("End of test \n");
 }
--- memasta.h
+++ memasta.h
@@ -3,28 +3,19 @@
 #include <stdint.h>
 #include <alloca.h>
 #include <stdio.h>
-
-// #define MM_MAX_BUCKET_SIZE 1024
+#include <string.h>
 
 struct mm_seg
 {
      size_t size;
-     int* mem; // memory were pointing to
+     int* mem; // memory we're pointing to
+     struct mm_seg* prev;
+     struct mm_seg* next;
 };
 
-void mm_free(void* seg);
 
 void* mm_alloc(size_t size);
 
+void mm_free(void* seg);
 
 
-// Simple as linked list
-// #define list(t_) struct {t_* head; t_* tail;}
-
-
-
-// This is probably the incorrect way to declare a custom type or something
-// typedef struct mm_list(t_ something, )
-// {
-//
-// };
