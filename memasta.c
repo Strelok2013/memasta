dbe05761d428ab1c9ee63b2b877d515c8cb5533d
Strelok_2012 <uraniumpuppy@gmail.com>
2024-02-14 13:56:42 +1100

    Functioning list push and pop
    Its not the most glamorous but it works. The hext hardest thing to implement would be the insert function.

--- memasta.c
+++ memasta.c
@@ -19,6 +19,57 @@ void* mm_alloc(size_t size)
     return seg;
 }
 
+void mm_list_init(struct mm_List l)
+{
+    memset(&l, 0, sizeof l);
+}
+
+void mm_list_push(mm_list *l, struct mm_seg *n)
+{
+    printf("Addr of list in push func: %p \n", &l);
+    if(!n)
+    {
+       return;
+    }
+
+    if(!l->head)
+    {
+        l->head = n;
+    }
+    else
+    {
+        if(!l->tail)
+        {
+            n->prev = l->head;
+            l->head->next = n;
+            l->tail = n;
+        }
+        else
+        {
+            n->prev = l->tail;
+            l->tail->next = n;
+            l->tail = n;
+        }
+    }
+}
+
+void mm_list_pop(mm_list *l)
+{
+    printf("Addr of list in pop func: %p \n", &l);
+    if(!l->tail)
+    {
+        if(l->head)
+        {
+            mm_free(l->head);
+        }
+        return;
+    }
+
+    l->tail->prev->next = NULL;
+    mm_free(l->tail);
+}
+
+
 
 void mm_free(void* seg)
 {
@@ -30,5 +81,13 @@ void mm_free(void* seg)
 
 int main(void)
 {
+    mm_list list = {0};
+    mm_list_init(list);
+
+    printf("Addr of list: %p \n", &list);
+
+    mm_list_push(&list, mm_alloc(64));
+    mm_list_pop(&list);
+
     printf("End of test \n");
 }
